d577904d8b3ece9d55d3c9f12ee9ca1206541808
gangukumbar <66021470+gangukumbar@users.noreply.github.com>
2021-05-04 10:52:31 +0530

    Add files via upload

---
+++ port.js
@@ -0,0 +1,12 @@
+//Aside Navbar
+
+const nav=document.querySelector(".nav"),
+navList=nav.querySelectorAll("li"),
+totalNavList=navList.length;
+
+for(let i=0; i<totalNavList;i++){
+	const a=navList[i].querySelector("a");
+	a.addEventListener("click", function(){
+		console.log(this);
+	})
+}
